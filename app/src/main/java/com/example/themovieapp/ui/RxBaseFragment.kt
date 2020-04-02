26c95551db01aa2bcd7c3aa1b8552a005a8461a1
임건혁 <gunhyuck11@naver.com>
2020-04-02 23:01:43 +0900

    RxBaseFragment

---
+++ app/src/main/java/com/example/themovieapp/ui/RxBaseFragment.kt
@@ -0,0 +1,16 @@
+package com.example.themovieapp.ui
+
+open class RxBaseFragment : Fragment() {
+
+    protected var subscriptions = CompositeDisposable()
+
+    override fun onResume() {
+        super.onResume()
+        subscriptions = CompositeDisposable()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        subscriptions.clear()
+    }
+}
